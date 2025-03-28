447d6f9406b2739c6d86891ecacab52a8f4b9a34
bibhashhalder <halderbibhash80@gmail.com>
2025-03-28 15:05:45 +0600

    admin created done

---
+++ src/app/config/modules/admin/admin.interface.ts
@@ -0,0 +1,39 @@
+import { Model, Types } from 'mongoose';
+
+export type IGender = 'male' | 'female' | 'other';
+export type IBloodGroup =
+  | 'A+'
+  | 'A-'
+  | 'B+'
+  | 'B-'
+  | 'AB+'
+  | 'AB-'
+  | 'O+'
+  | 'O-';
+
+export type TUserName = {
+  firstName: string;
+  middleName: string;
+  lastName: string;
+};
+
+export type TAdmin = {
+  id: string;
+  user: Types.ObjectId;
+  designation: string;
+  name: TUserName;
+  gender: IGender;
+  dateOfBirth?: Date;
+  email: string;
+  contactNo: string;
+  emergencyContactNo: string;
+  bloogGroup?: IBloodGroup;
+  presentAddress: string;
+  permanentAddress: string;
+  profileImg?: string;
+  isDeleted: boolean;
+};
+
+export interface AdminModel extends Model<TAdmin> {
+  isUserExists(id: string): Promise<TAdmin | null>;
+}
